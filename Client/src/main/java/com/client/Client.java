7f49a627683ecdc3b3f150d7311dc9e50683fe82
Jyberion <codelyoko197@gmail.com>
2023-11-23 04:52:10 -0500

    Update Client.java

--- Client/src/main/java/com/client/Client.java
+++ Client/src/main/java/com/client/Client.java
@@ -1,6 +1,7 @@
 import com.client.auth.AuthenticationHandler;
 import com.client.auth.AuthServerMessageType;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -13,43 +14,48 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import java.net.InetSocketAddress;
+import javax.net.ssl.SSLContext;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        // Create and configure SSLContext
-        SslContext sslContext = SslContextBuilder.forClient()
-                .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                .build();
+public static void main(String[] args) throws Exception {
+    // Create and configure SSLContext
+    SslContext sslContext = SslContextBuilder.forClient()
+            .trustManager(InsecureTrustManagerFactory.INSTANCE)
+            .build();
 
-        // Create AuthenticationHandler instance
-        AuthenticationHandler authenticationHandler = new AuthenticationHandler(sslContext);
+    // Create AuthenticationHandler instance
+            SSLContext javaSslContext = sslContext.newEngine(PooledByteBufAllocator.DEFAULT)
+            .getSSLContext();
 
-        // Configure the client
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    .remoteAddress(new InetSocketAddress("localhost", 8080))
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) {
-                            ch.pipeline().addLast(authenticationHandler);
-                        }
-                    });
+        AuthenticationHandler authenticationHandler = 
+            new AuthenticationHandler(javaSslContext);
 
-            // Start the client
-            ChannelFuture channelFuture = bootstrap.connect().sync();
-            Channel channel = channelFuture.channel();
+    // Configure the client
+    EventLoopGroup group = new NioEventLoopGroup();
+    try {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .remoteAddress(new InetSocketAddress("localhost", 8080))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(authenticationHandler);
+                    }
+                });
 
-            // Perform any additional tasks with the channel if needed
+        // Start the client
+        ChannelFuture channelFuture = bootstrap.connect().sync();
+        Channel channel = channelFuture.channel();
 
-            // Wait for the channel to close
-            channel.closeFuture().sync();
-        } finally {
-            // Shutdown the event loop group
-            group.shutdownGracefully().sync();
-        }
+        // Perform any additional tasks with the channel if needed
+
+        // Wait for the channel to close
+        channel.closeFuture().sync();
+    } finally {
+        // Shutdown the event loop group
+        group.shutdownGracefully().sync();
     }
+}
 }
